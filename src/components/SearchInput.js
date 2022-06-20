c5c662abfb838b91dda048b430bb4e0a183f0446
Ayellet Tsachi <ayelletts@gmail.com>
2022-06-20 15:42:31 +0300

    useRef

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,36 +1,33 @@
-//import { useContext } from "react";
-//import SearchContext from "./Main";
-
 import React, { useRef } from "react";
-
-export default function SearchInput(props) {
-  const onChange = (e) => {
-    props.searchState(e.target.value);
-  };
-
-  return (
-    <>
-      <input
-        id="searchInput"
-        placeholder="Enter song name"
-        onChange={onChange}
-      />
-    </>
-  );
-}
-
 // export default function SearchInput(props) {
-//   const refSearch = useRef();
-
-//   const onClick = (ref) => {
-//     // props.searchState(ref.current.value);
-//     props.searchState(document.getElementById("searchInput").value);
+//   const onChange = (e) => {
+//     props.searchState(e.target.value);
 //   };
 
 //   return (
 //     <>
-//       <input id="searchInput" ref={refSearch} placeholder="Enter song name" />
-//       <button onClick={onClick(refSearch)}>Search</button>
+//       <input
+//         id="searchInput"
+//         placeholder="Enter song name"
+//         onChange={onChange}
+//       />
 //     </>
 //   );
 // }
+
+export default function SearchInput(props) {
+  const refSearch = useRef(0);
+
+  const btnSearchClick = (ref) => {
+    props.searchState(ref.current.value);
+  };
+
+  return (
+    <>
+      <input id="searchInput" ref={refSearch} placeholder="Enter song name" />
+      <button id="btnSearch" onClick={btnSearchClick(refSearch)}>
+        Search
+      </button>
+    </>
+  );
+}
